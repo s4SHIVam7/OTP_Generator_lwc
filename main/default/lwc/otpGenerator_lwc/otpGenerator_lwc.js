58cb8f8b2d20c814d5fddab0fa7d64e519b6d3f6
Shivam Laidwar <60181328+s4SHIVam7@users.noreply.github.com>
2023-09-26 01:50:46 +0530

    Otp generator2 (#2)
    * updated js file

--- main/default/lwc/otpGenerator_lwc/otpGenerator_lwc.js
+++ main/default/lwc/otpGenerator_lwc/otpGenerator_lwc.js
@@ -1,5 +1,30 @@
-import { LightningElement } from 'lwc';
-export default class OtpGenerator_lwc extends LightningElement {
+import { LightningElement, api } from 'lwc';
+export default class OtpGenerator extends LightningElement {
+    @api otpLength = 5;
+    @api timeDuration = 30;
+    generatedOTP ='';
+    disabledButton = false;
+    showTimer = false;
+    timerText = "";
+    handleClick() {
+        let otpData=[];
+        for(let  i=1; i<this.otpLength; i++) {
+            otpData.push(Math.floor(Math.random()*10));
+        }
+        this.generatedOTP = otpData.join('');
+        this.disabledButton = true;
+        this.showTimer = true;
 
-
+        let remainingSeconds = this.timeDuration;
+        let countDownInterval = setInterval(()=>{
+            remainingSeconds --;
+            this.timerText =`TO generate next OTP, wait for ${remainingSeconds} seconds`;
+            if(remainingSeconds <= 0) {
+                clearInterval(countDownInterval);
+                this.disabledButton = false;
+                this.showTimer = false;
+            }
+        },1000)
+        
+    }
 }
